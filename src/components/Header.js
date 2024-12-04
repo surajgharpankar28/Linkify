a3f69a7e98c9513bc279d01233ec540aa087f31e
Suraj Gharpankar <surajgharpankar28@gmail.com>
2024-12-04 16:35:02 +0530

    added Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "lucide-react";
 
 const Header = () => {
   return (
-    <div>
-      <h1 className="text-2xl poppins-semibold-italic">Linkify</h1>
+    <div className="flex items-center justify-between bg-white p-2 w-[80vw] absolute top-10 right-[10vw] rounded-full px-6">
+      <div>
+        <h1 className="flex items-center text-3xl poppins-semibold-italic">
+          <Link className="mx-1" />
+          Linkify
+        </h1>
+      </div>
+      <div className="flex gap-4">
+        <button className="border bg-gray-300 font-semibold p-2 rounded-lg">
+          Log In
+        </button>
+        <button className="border bg-gray-800 font-semibold text-white p-2 rounded-lg">
+          Sign up free
+        </button>
+      </div>
     </div>
   );
 };
