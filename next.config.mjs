23293664a99f9f0b4e5dd113e5ad227f0071d2be
Suraj Gharpankar <surajgharpankar28@gmail.com>
2024-12-10 12:18:29 +0530

    added all remote allowed links

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["media.licdn.com", "scontent.fnag4-2.fna.fbcdn.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
   },
 };
 
